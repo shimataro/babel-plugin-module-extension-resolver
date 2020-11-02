330d1cb18f764fea96dbd2c76e6993e30c0d865b
rozzzly <pclienau@gmail.com>
2020-11-01 18:44:10 -0600

    add async import to examples

--- examples/ts-babel-cjs/src/index.ts
+++ examples/ts-babel-cjs/src/index.ts
@@ -3,6 +3,10 @@ import * as mod from "./dir/mod";
 
 export * from "./dir/mod";
 
+async function foo() {
+	const foo = await import("./dir/mod");
+}
+
 for(const x of [[1, 2], 3, [4, 5]].flat())
 {
 	mod.foo(x);
--- examples/ts-babel-esm/src/index.ts
+++ examples/ts-babel-esm/src/index.ts
@@ -1,7 +1,12 @@
+
 import "./dir";
 import * as mod from "./dir/mod";
 
 export * from "./dir/mod";
+
+async function foo() {
+	const foo = await import("./dir/mod");
+}
 
 for(const x of [[1, 2], 3, [4, 5]].flat())
 {
